96c15475b210359671a9bd59b71a94bf156468f2
kiruhafighter <salomatin.kirill@itperspectives.ro>
2022-12-13 23:54:41 +0200

    Add Dependency Injection

---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,21 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public PokemonRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection <Pokemon> GetPokemons()
+        {
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+
+    }
+}
